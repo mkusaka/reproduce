c9359eadc0be7854532e126c017273f93bf89f5b
mkusaka <hinoshita1992@gmail.com>
2020-11-01 19:08:22 +0900

    feat: use deepcopy and update deps

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
+import * as deepcopy from 'deepcopy';
+
 interface Handler<T, R> {
   (arg: T): R;
 }
 
 export function reproduce<S extends object, R>(state: S, func: Handler<S, R>) {
-  const copy = {
-    ...state,
-  };
+  const copy = deepcopy(state);
   func(copy);
   return copy;
 }
